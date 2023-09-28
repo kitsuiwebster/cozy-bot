8f9168ba7b8c3eb0972a5a0749a78f357ab87839
kitsuiwebster <raphmartin1999@gmail.com>
2023-09-29 01:09:28 +0200

    feat: slash commands

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import logging
 import os
 from dotenv import load_dotenv
 
+
 # Load environment variables from .env file
 load_dotenv()
 
@@ -15,39 +16,31 @@ intents.typing = False
 intents.message_content = True  
 
 # Initialize Poof Poof
-bot = commands.Bot(command_prefix=commands.when_mentioned_or('!'), intents=intents)
+bot = commands.Bot(command_prefix=commands.when_mentioned_or('/'), intents=intents)
 
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} is connected !!')
     bot.heartbeat_interval = 360
+    await bot.sync_commands()
 
 @bot.event
 async def on_message(message):
     # Avoid responding to the bot's own messages
     if message.author == bot.user:
         return
-
-    # Add your logic for determining when to respond to user messages here
-    if message.content.startswith('!your_command_prefix'):
-        # Handle your bot's responses here
-        response = "Your response here"
-        await message.channel.send(response)
-
     await bot.process_commands(message)
 
-
-# Change the status every 10s
 async def change_status():
     await bot.wait_until_ready()
 
     while not bot.is_closed():
-        discord.Game("!rain"),
+        discord.Game("🌧️"),
 
 async def run_bot():
     try:
-        await bot.load_extension('commands.rain')
+        bot.load_extension('commands.rain')
 
     except Exception as e:
         print(f"Error loading extension: {e}")
--- reactions.py
+++ reactions.py
@@ -5,18 +5,9 @@ async def add_reactions(message, reactions):
 async def handle_reactions(message):
     clean_message = message.content.lower()
     if (
-        "!story" in clean_message or
-        "!softstory" in clean_message or
-        "!dialogue" in clean_message or
-        "!dilemma" in clean_message or
-        "!situation" in clean_message or
-        "!encode" in clean_message or
-        "!dihia" in clean_message or
-        "!cmd" in clean_message or
-        "!poof" in clean_message or
-        "!ask" in clean_message or
-        "!join" in clean_message or
-        "!leave" in clean_message or
-        "!rain" in clean_message
+        "pluie" in clean_message or
+        "rain" in clean_message or
+        "goutte" in clean_message or
+        "raindrop" in clean_message
     ):
-        await add_reactions(message, ["✅"])
+        await add_reactions(message, ["🌧️"])
