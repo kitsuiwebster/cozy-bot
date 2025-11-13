bfdf486e0a1ad45bbb0cba26e007effeded522a9
kitsuiwebster <raphmartin1999@gmail.com>
2025-11-13 02:41:14 +0100

    feat: ssl

--- server.py
+++ server.py
@@ -24,7 +24,7 @@ def start_api():
         uvicorn.run(
             "api.app:app",
             host="0.0.0.0",
-            port=8443,  # HTTPS port
+            port=8000,  # HTTPS port
             ssl_keyfile=ssl_keyfile,
             ssl_certfile=ssl_certfile,
             log_level="info"
