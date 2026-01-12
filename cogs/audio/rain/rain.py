1b3f2e894e83e1789d53cc0db52f6d0293a5c064
kitsuiwebster <raphmartin1999@gmail.com>
2026-01-12 23:54:34 +0100

    feat: rain06 rain08 & rain09

--- cogs/audio/rain/rain.py
+++ cogs/audio/rain/rain.py
@@ -5,13 +5,18 @@ from ..base_sound import BaseSoundCog
 
 class RainCog(BaseSoundCog):
     def __init__(self, bot):
-        sounds = ["rain00.mp3", "rain01.mp3", "rain02.mp3", "rain03.mp3", "rain04.mp3"]
+        sounds = ["rain00.mp3", "rain01.mp3", "rain02.mp3", "rain03.mp3", "rain04.mp3", "rain05.mp3", "rain06.mp3", "rain07.mp3", "rain08.mp3", "rain09.mp3"]
         sound_labels = {
             "rain00.mp3": "🌧️💧⚡",
             "rain01.mp3": "🌧️🌿🌙",
             "rain02.mp3": "🌧️⛈️💨",
             "rain03.mp3": "🌧️🏠🔥",
             "rain04.mp3": "🌧️🚗⚡",
+            "rain05.mp3": "🌧️🌧️🌧️",
+            "rain06.mp3": "🌧️🐦🌿",
+            "rain07.mp3": "🌧️🌧️🌧️",
+            "rain08.mp3": "🌧️🔥⛺",
+            "rain09.mp3": "🌧️🧚🏻‍♀🌲",
         }
         super().__init__(bot, "rain", sounds, sound_labels, "Play the sound of rain.🌧️")
 
