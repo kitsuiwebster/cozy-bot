3897f8d8b64e4343ee4638f0372a07ed4dda8406
kitsuiwebster <raphmartin1999@gmail.com>
2026-01-24 01:16:00 +0000

    debug: enable Discord DEBUG logs for troubleshooting

--- prod/main.py
+++ prod/main.py
@@ -64,7 +64,7 @@ handler.setFormatter(FancyFormatter())
 logger.addHandler(handler)
 discord_logger.propagate = False
 discord_logger.addHandler(handler)
-discord_logger.setLevel(logging.ERROR)
+discord_logger.setLevel(logging.DEBUG)  # Changed from ERROR to DEBUG for detailed Discord logs
 
 # Configure Discord bot intents
 intents = discord.Intents.default()
