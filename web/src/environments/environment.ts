df3fd981ec72dcc03078a3909aff357050d4f4cd
kitsuiwebster <raphmartin1999@gmail.com>
2026-02-01 07:54:29 +0100

    fix: cors

--- web/src/environments/environment.ts
+++ web/src/environments/environment.ts
@@ -1,5 +1,5 @@
 // Development environment - utilise l'API distante pour dev
 export const environment = {
   production: false,
-  apiUrl: 'https://api.cozybot.online:8000/api'
+  apiUrl: 'https://api.cozybot.online/api/public'
 };
